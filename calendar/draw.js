85422ccbf4158ab463d296badf651dde616e3a38
fluffyowl <nebukuro09@gmail.com>
2017-12-03 21:11:02 +0900

    Add api request

---
+++ calendar/draw.js
@@ -0,0 +1,21 @@
+(function(){
+  var requestAjax = function(username, callback) {
+    var xhr = new XMLHttpRequest();
+    var endpoint = "http://kenkoooo.com/atcoder-api/problems?user=" + name;
+    xhr.onreadystatechange = function(){
+      if (this.readyState==4 && this.status==200) {
+        callback(this.response);
+      }
+    };
+    xhr.responseType = 'json';
+    xhr.open('GET',endpoint,true);
+    xhr.send();
+  };
+})();
+
+requestAjax("nebukuro09", function(response){
+  console.log(response);
+});
+
+var cal = new CalHeatMap();
+cal.init({});
